fd71be7fa90c2269667e95ea79134c2011801394
majed <m.a.a.almaged@gmail.com>
2022-12-08 22:24:13 +0300

    basic

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import * as React from "react"
+import {
+  ChakraProvider,
+  theme,
+} from "@chakra-ui/react"
+import Router from "./components/routes/router";
+export const App = () => (
+ 
+ 
+<ChakraProvider theme={theme}>
+
+<Router/>
+
+</ChakraProvider>
+
+
+
+
+
+
+
+)
